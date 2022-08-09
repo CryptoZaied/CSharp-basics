5de3c87ddb422df7c4264cedcadbd8186df751d7
Abdullah Al Zaied <Al.Zaied@brainstation-23.com>
2022-08-10 04:35:40 +0600

    Task 2 done

--- Program.cs
+++ Program.cs
@@ -5,6 +5,23 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            bool isIt = true;
+            Console.WriteLine($"Printing bool value: {isIt}");
+            int Number = int.MaxValue;
+            Console.WriteLine($"Printing maximum integer data: {Number}");
+            long BigNumber = long.MaxValue;
+            Console.WriteLine($"Printing maximum long data: {BigNumber}");
+            decimal DecimalNumber = decimal.MaxValue;
+            Console.WriteLine($"Printing maximum decimal data: {DecimalNumber}");
+            float Fraction = float.MaxValue;
+            Console.WriteLine($"Printing maximum float data: {Fraction}");
+            double BigFraction = double.MaxValue;
+            Console.WriteLine($"Printing maximum double data: {BigFraction}");
+            char Symbol = 'C';
+            Console.WriteLine($"Printing a character: {Symbol}");
+            string Sequence = "I am the Author";
+            Console.WriteLine($"Printing an string: {Sequence}");
         }
     }
 }
